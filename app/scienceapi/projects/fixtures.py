b9b30839f86552ff4284826cf9c56fc859c40139
alicoding <ali@alicoding.com>
2016-04-20 10:30:03 -0400

    Add test

---
+++ app/scienceapi/projects/fixtures.py
@@ -0,0 +1,3 @@
+data = {
+    'blah': True
+}
--- app/scienceapi/projects/tests.py
+++ app/scienceapi/projects/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+import unittest
+from django.test import Client
+from . import fixtures
 
-# Create your tests here.
+
+class ProjectTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_get_all_projects(self):
+        response = self.client.get('/projects/')
+
+        self.assertEqual(response.json(), fixtures.data)
+        self.assertEqual(response.status_code, 200)
